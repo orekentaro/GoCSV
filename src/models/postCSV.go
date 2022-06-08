f79336a454c45f6a35ebb18bc733ff31cf003324
orekentaro <kntru0218gj@gmail.com>
2022-06-08 19:07:11 +0900

    ファイルオープンまでできた

--- src/models/postCSV.go
+++ src/models/postCSV.go
@@ -1,9 +1,12 @@
 package models
 
 import (
+	"encoding/csv"
 	"fmt"
 
 	"github.com/gin-gonic/gin"
+	"golang.org/x/text/encoding/japanese"
+	"golang.org/x/text/transform"
 )
 
 func PostCSV(c *gin.Context) {
@@ -11,6 +14,22 @@ func PostCSV(c *gin.Context) {
 	csvData := c.Request.MultipartForm.File["csvData"][0]
 	outputType := c.PostForm("outputType")
 	fmt.Println(header, csvData, outputType)
+
+	file, err := csvData.Open()
+	if err != nil {
+		c.JSON(200, gin.H{
+			"message": err,
+		})
+	}
+
+	r := csv.NewReader(transform.NewReader(file, japanese.ShiftJIS.NewDecoder()))
+	for {
+		records, err := r.Read()
+		if err != nil {
+			break
+		}
+		fmt.Println(records)
+	}
 	c.JSON(200, gin.H{
 		"message": "connect",
 	})
