918062d4d34aafe75dfe1a24874f7614d3e72065
orekentaro <kntru0218gj@gmail.com>
2022-06-09 19:20:55 +0900

    CSV 列選択

--- src/models/postCSV.go
+++ src/models/postCSV.go
@@ -12,6 +12,7 @@ func PostCSV(c *gin.Context) {
 	header := strings.Split(c.PostForm("header"), ",")
 	csvData := c.Request.MultipartForm.File["csvData"][0]
 	outputType := c.PostForm("outputType")
+	fmt.Println((outputType))
 
 	file, err := csvData.Open()
 	if err != nil {
@@ -20,11 +21,12 @@ func PostCSV(c *gin.Context) {
 		})
 	}
 
-	var record string
+	var records string
 
 	r := csv.NewReader(file)
 
 	csvHeader, err := r.Read()
+	// records += strings.Join(csvHeader, ",") + "\n"
 	if err != nil {
 		c.JSON(200, gin.H{
 			"message": err,
@@ -39,21 +41,29 @@ func PostCSV(c *gin.Context) {
 		}
 	}
 
-	fmt.Println(outputType)
-	fmt.Println(csvHeader)
-	fmt.Println(header)
-	fmt.Println(indexList)
+	recordsList := []string{}
+	for _, num := range indexList {
+		recordsList = append(recordsList, csvHeader[num])
+	}
+	records += strings.Join(recordsList, ",") + "\n"
+
 	for {
 		row, err := r.Read()
 		if err != nil {
 			break
 		}
-		record += strings.Join(row, ",") + "\n"
+
+		recordsList := []string{}
+
+		for _, num := range indexList {
+			recordsList = append(recordsList, row[num])
+		}
+		records += strings.Join(recordsList, ",") + "\n"
 	}
 
 	c.Writer.Header().Set("Content-Disposition", "attachment; filename=test.csv")
 	c.Writer.Header().Set("Content-Type", "text/csv")
-	c.Writer.Write([]byte(record))
+	c.Writer.Write([]byte(records))
 }
 
 func include(slice []string, target string) bool {
