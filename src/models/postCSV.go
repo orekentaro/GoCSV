8c040579b1a670c6656a0f94c71a51373a897013
orekentaro <kntru0218gj@gmail.com>
2022-06-09 14:55:46 +0900

    選択されたヘッダーのインデックスを取得

--- src/models/postCSV.go
+++ src/models/postCSV.go
@@ -9,10 +9,9 @@ import (
 )
 
 func PostCSV(c *gin.Context) {
-	header := c.PostForm("header")
+	header := strings.Split(c.PostForm("header"), ",")
 	csvData := c.Request.MultipartForm.File["csvData"][0]
 	outputType := c.PostForm("outputType")
-	fmt.Println(header, csvData, outputType)
 
 	file, err := csvData.Open()
 	if err != nil {
@@ -24,6 +23,26 @@ func PostCSV(c *gin.Context) {
 	var record string
 
 	r := csv.NewReader(file)
+
+	csvHeader, err := r.Read()
+	if err != nil {
+		c.JSON(200, gin.H{
+			"message": err,
+		})
+	}
+
+	indexList := []int{}
+
+	for index, value := range csvHeader {
+		if include(header, value) {
+			indexList = append(indexList, index)
+		}
+	}
+
+	fmt.Println(outputType)
+	fmt.Println(csvHeader)
+	fmt.Println(header)
+	fmt.Println(indexList)
 	for {
 		row, err := r.Read()
 		if err != nil {
@@ -31,9 +50,17 @@ func PostCSV(c *gin.Context) {
 		}
 		record += strings.Join(row, ",") + "\n"
 	}
-	fmt.Println(record)
 
 	c.Writer.Header().Set("Content-Disposition", "attachment; filename=test.csv")
 	c.Writer.Header().Set("Content-Type", "text/csv")
 	c.Writer.Write([]byte(record))
+}
+
+func include(slice []string, target string) bool {
+	for _, value := range slice {
+		if value == target || "\ufeff"+value == target {
+			return true
+		}
+	}
+	return false
 }
